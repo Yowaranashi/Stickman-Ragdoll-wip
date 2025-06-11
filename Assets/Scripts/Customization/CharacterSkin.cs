ef852fdb79459b63fea78e1d7fab7f4b9ccdf975
Yowari <yowari.san@gmail.com>
2025-06-11 15:12:22 +0300

    Remove ad SDK and adapt for Android

--- Assets/Scripts/Customization/CharacterSkin.cs
+++ Assets/Scripts/Customization/CharacterSkin.cs
@@ -5,10 +5,8 @@ public class CharacterSkin : ScriptableObject
 {
     public string Key;
     [SerializeField] private Sprite _head, _body, _handLeft, _handRight, _legLeft, _legRight;
-    [SerializeField] private bool _openForAd;
     [SerializeField] private int _cost = 100;
     public bool Opened, Chosen;
-    public bool OpenForAd => _openForAd;
     public int Cost => _cost;
     public Sprite Head => _head;
     public Sprite Body => _body;
